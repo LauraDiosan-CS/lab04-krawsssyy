0dd7c4ab4d54f2c4f8a04822cf9b96dc67b5899f
krawsssyy <38566149+krawsssyy@users.noreply.github.com>
2020-03-29 21:14:36 +0300

    Add files via upload

--- repo.cpp
+++ repo.cpp
@@ -1,62 +1,61 @@
-#include "Repo.h"
-
-Repo::Repo() {
-	this->n = 0;
-}
-Repo::~Repo() {
-	this->n = 0;
-	
-}
-void Repo::addElement(Player p) {
-	this->players[this->n++] = p;
-}
-
-Player* Repo::getAll() {
-	return this->players;
-}
-
-int Repo::getSize()
-{
-	return this->n;
-}
-
-void Repo::updatePlayer(int id, const Player& p)
-{
-	for (int i = 0; i < this->getSize(); i++)
-		if (this->getAll()[i].getId() == id)
-			this->getAll()[i] = p;
-}
-
-Player* Repo::readPlayer(int id)
-{
-	if (id)
-	{
-		for (auto player : this->players)
-			if (player.getId() == id)
-			{
-				Player* pp = new Player(player);
-				return pp;
-			}
-		return nullptr;
-	}
-	else
-		return this->players;
-}
-
-void Repo::deletePlayer(int id)
-{
-	int i;
-	int p = this->getSize();
-	for (i = 0; i < p; i++)
-		if (this->players[i].getId() == id)
-			break;
-	if (i < p)
-	{
-		for (int j = i; j < p - 1; j++)
-			this->players[j] = this->players[j + 1];
-		this->n--;
-
-	}
-
-
-}
+//#include "Repo.h"
+//#include <vector>
+//using namespace std;
+//
+//template <class T> Repo<T>::Repo() {
+//	;
+//}
+//template <class T> Repo<T>::~Repo() {
+//	this->entities.clear();
+//	
+//}
+//template <class T> void Repo<T>::addElement(T& s) {
+//	this->entities.push_back(s);
+//}
+//
+//template<class T> vector<T> Repo<T>::getAll() {
+//	return this->entities;
+//}
+//
+//template <class T> int Repo<T>::getSize()
+//{
+//	return this->entities.size();
+//}
+//
+//template <class T>void Repo<T>::updateEntity(int id, const T& p)
+//{
+//	for (int i = 0; i < this->entities.size(); i++)
+//		if (this->entities[i].getID() == id)
+//			this->entities[i] = p;
+//}
+//
+//template <class T> T* Repo<T>::readEntity(int id)
+//{
+//	if (id)
+//	{
+//		for (auto entity : this->entities)
+//			if (entity.getId() == id)
+//			{
+//				T* t = new T(entity);
+//				return t;
+//			}
+//		return nullptr;
+//	}
+//	else
+//		return this->entities;
+//}
+//
+//template <class T> void Repo<T>::deleteEntity(int id)
+//{
+//	int i;
+//	int p = this->entities.size();
+//	for (i = 0; i < p; i++)
+//		if (this->entities[i].getId() == id)
+//			break;
+//	if (i < p)
+//	{
+//		this->entities.erase(this->entities.begin() + i);
+//	}
+//
+//
+//}
